d5c7f36b75e99da17f7682ddca4d973439cd586b
Paulo Vitorino <pavf.paulo@gmail.com>
2021-08-05 01:25:41 -0300

    Auth style

--- src/Body.php
+++ src/Body.php
@@ -1,29 +1,26 @@
-<div>
+<div id="page-auth">
     <aside>
         <strong>String Calculator</strong>
         <p>A solution for 7Shifts Technical Interview by Paulo Vitorino.</p>
         <p>Made with Vanilla Javascript, PHP and CSS</p>
     </aside>
     <main>
-        <div>
-            <img src="./src/assets/images/logo.svg" alt="7Shifts Logo" />
-            <form onSubmit="event.preventDefault(); handleSolution();">
-                <div>
+        <div class="main-content">
+            <img id="logo" src="./src/assets/images/logo.svg" alt="7Shifts Logo" />
+                <form id="formbox" onSubmit="event.preventDefault(); handleSolution();">
                     <textarea
                         placeholder='Type your string here.'
                         name='inputValue'
                         id='inputValue'
-                        onInput="blankSolution();"
-                    ></textarea>
-                </div>
-                <div>
+                        onInput="blankSolution();"></textarea>
                     <button type='button' onClick="handleSolution();">Add</button>
-                </div>
-            <div>Solution</div>
-            <div>
-                <p id="solution"> </p>
-            </div>
-            </form>
+                    <div id="solutionbox">
+                        <div class="separator">Solution</div>
+                        <div>
+                            <p id="solution"> </p>
+                        </div>
+                    </div>
+                </form>
         </div>
     </main>
 </div >
--- src/MetaHead.php
+++ src/MetaHead.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
 <link rel="stylesheet" href="./src/styles/global.css?v=<?=filemtime('./src/styles/global.css');?>">
+<link rel="stylesheet" href="./src/styles/auth.css?v=<?=filemtime('./src/styles/auth.css');?>">
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
